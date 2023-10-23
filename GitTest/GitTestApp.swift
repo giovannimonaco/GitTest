68afee3e2657ccd23f0f91dfcfbd3aad3a1b9eed
Giovanni Monaco <33128632+giovannimonaco@users.noreply.github.com>
2023-10-23 17:43:44 +0200

    First Commit
    First Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Giovanni Monaco on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
