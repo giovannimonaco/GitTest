3dfd810717cbe3e9b5e28cd008851ecce649399a
Giovanni Monaco <33128632+giovannimonaco@users.noreply.github.com>
2023-10-24 09:55:01 +0200

    Update Card

--- GitTest/CardView.swift
+++ GitTest/CardView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 20)
+            .foregroundStyle(.pink)
     }
 }
 
