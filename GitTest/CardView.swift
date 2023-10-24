6f3a3ca9dba7115fd12ca5e72bd83cff19e7f739
Giovanni Monaco <33128632+giovannimonaco@users.noreply.github.com>
2023-10-24 09:44:12 +0200

    Added CardView File

---
+++ GitTest/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  GitTest
+//
+//  Created by Giovanni Monaco on 24/10/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
